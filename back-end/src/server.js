78d0d2b0e5102dd1a3756e08c0c8507a71f7aed6
azizx018 <chiara.aziz@gmail.com>
2023-10-11 16:32:54 -0500

    delete end point created

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,14 @@
-import { cartItems, products } from './temp-data';
 import express from 'express';
+import { cartItems as cartItemsRaw, products as productsRaw } from './temp-data';
+
+let cartItems = cartItemsRaw;
+let products = productsRaw;
+
 
 const port = 8000;
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (req, res) => {
     res.send("Hello Jello!")
@@ -20,16 +25,28 @@ app.get('/cart', (req, res) => {
 });
 
 app.get('/products/:productId', (req, res) => {
-    const productId = re.params.productId;
+    const productId = req.params.productId;
     const product = products.find(product => product.id === productId);
     res.json(product);
 
 });
 
+app.post('/cart', (req, res) => {
+    const productId = req.body.id;
+    const product = products.find(product => product.id === productId);
+    cartItems.push(product);
+    res.json(cartItems);
+
+});
+
+app.delete('/cart/:productId', (req, res) => {
+    const productId = req.params.productId;
+    cartItems = cartItems.filter(product => product.id !== productId);
+    res.json(cartItems);
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-
-
 });
 
 
