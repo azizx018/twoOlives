cdac881be63379ef848a0051c7d15217b79c753b
azizx018 <chiara.aziz@gmail.com>
2023-10-11 17:20:27 -0500

    edited cart to make more simple

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,4 +1,6 @@
-import express from 'express';
+import express, { request } from 'express';
+
+//just for editing at the moment since there is no db
 import { cartItems as cartItemsRaw, products as productsRaw } from './temp-data';
 
 let cartItems = cartItemsRaw;
@@ -9,6 +11,7 @@ const port = 8000;
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/hello', (req, res) => {
     res.send("Hello Jello!")
@@ -18,9 +21,14 @@ app.get('/products', (req, res) => {
     res.json(products);
 
 });
+function populateCartIds(ids) {
+    return ids.map(id => products.find(product => product.id === id))
+    
+}
 
 app.get('/cart', (req, res) => {
-    res.json(cartItems);
+    const populatedCart = populateCartIds(cartItems);
+    res.json(populatedCart);
 
 });
 
@@ -31,18 +39,24 @@ app.get('/products/:productId', (req, res) => {
 
 });
 
-app.post('/cart', (req, res) => {
-    const productId = req.body.id;
-    const product = products.find(product => product.id === productId);
-    cartItems.push(product);
-    res.json(cartItems);
+// app.post('/lulu', (req, res) => {
+//     const data = request.body;
+//     console.log(data.id);
 
+// });
+
+app.post('/cart', (req, res) => {
+    const data = req.body;
+    cartItems.push(data.id);
+    const populatedCart = populateCartIds(cartItems);
+    res.json(populatedCart);
 });
 
 app.delete('/cart/:productId', (req, res) => {
     const productId = req.params.productId;
-    cartItems = cartItems.filter(product => product.id !== productId);
-    res.json(cartItems);
+    cartItems = cartItems.filter(id => id !== productId);
+    const populatedCart = populateCartIds(cartItems);
+    res.json(populatedCart);
 });
 
 app.listen(port, () => {
