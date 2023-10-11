c467b00db4366dc711ed2616b030b2da7fd17e28
azizx018 <chiara.aziz@gmail.com>
2023-10-11 11:32:07 -0500

    app .vue page edited

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar/>
+  <div class="page-wrap">
+    <Router-view></Router-view>
+
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue';
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { NavBar }
 }
 </script>
 
