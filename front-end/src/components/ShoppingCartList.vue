5ade172d6e867cd1a2fbc393b8468ba659dee81b
azizx018 <chiara.aziz@gmail.com>
2023-10-11 11:28:36 -0500

    added page components

---
+++ front-end/src/components/ShoppingCartList.vue
@@ -0,0 +1,28 @@
+<template>
+   
+      <div 
+      class="product-container"
+      v-for="product in products"
+      :key="product.id"
+      >
+        <img class="product-image " :src="product.imageName"/>
+        <div class="details-wrap">
+          <h3>{{ product.name }}</h3>
+          <p>{{ product.price }}</p>
+        </div>
+        <button class="remove-button">Remove from Cart</button>
+      </div>
+      
+
+    
+
+</template>
+
+<script>
+import cartItems from '../temp-data';
+
+export default {
+  name: "ShoppingCartList",
+  props: ['products'],
+}
+</script>
