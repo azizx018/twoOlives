bf70a508cc40ac079122ed1cb18c2a5a58724cce
azizx018 <chiara.aziz@gmail.com>
2023-10-11 11:30:24 -0500

    Product detail page added

---
+++ front-end/src/pages/ProductDetailPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="product">
+    <div class="img-wrap">
+      <img :src="product?.imageName" />
+    </div>
+    <div class="product-details">
+      <h1 >{{ product?.name }}</h1>
+      <h3 class="price">{{ product?.price }}</h3>
+      <button class="add-to-cart">Add To Cart</button>
+    </div>
+  </div>
+  <div v-else="!product">
+    <NotFoundPage/>
+  
+  </div>
+ 
+  
+
+</template>
+
+<script>
+import NotFoundPage from './NotFoundPage.vue'
+import { products } from '../temp-data';
+export default {
+  name: "ProductDetailPage",
+  data() {
+    return {
+      product: products.find(product => product.id === this.$route.params.productId),
+    }
+  },
+  components: {
+    NotFoundPage
+  }
+};
+</script>
