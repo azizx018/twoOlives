d943730714908a1bc358310118949e000d282929
azizx018 <chiara.aziz@gmail.com>
2023-10-15 18:02:47 -0500

    all endpoints converted to use mongo

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -5,74 +5,85 @@ import 'dotenv/config';
 
 
 
-//just for editing at the moment since there is no db
-import { cartItems as cartItemsRaw, products as productsRaw } from './temp-data';
-
-let cartItems = cartItemsRaw;
-let products = productsRaw;
-
-const url = `mongodb+srv://fullstack-server:${process.env.DB_PASSWORD}@cluster0.toqxqs8.mongodb.net/?retryWrites=true&w=majority`
-
-const client = new MongoClient(url);
-
-const port = 8000;
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 
 
-app.get('/products', async (req, res) => {
-    await client.connect();
-    const db = client.db('fsv-db');
-    const products = await db.collection('products').find({}).toArray();
-    res.send(products);
-});
 
+async function start () {
+    const port = 8000;
+    const url = `mongodb+srv://fullstack-server:${process.env.DB_PASSWORD}@cluster0.toqxqs8.mongodb.net/?retryWrites=true&w=majority`
 
-async function populateCartIds(ids) {
-    await client.connect();
-    const db = client.db('fsv-db');
-    return Promise.all(ids.map(id => db.collection('products').findOne({id})));
+    const client = new MongoClient(url);  
     
+    await client.connect();
+    const db = client.db('fsv-db');
+
+    const app = express();
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
+
+
+    app.get('/products', async (req, res) => {
+        
+        const products = await db.collection('products').find({}).toArray();
+        res.send(products);
+    });
+
+
+    async function populateCartIds(ids) {
+        return Promise.all(ids.map(id => db.collection('products').findOne({id})));
+        
+    }
+
+    app.get('/users/:userId/cart', async (req, res) => {
+        const user = await db.collection('users').findOne({ id: req.params.userId});
+        const populatedCart = await populateCartIds(user.cartItems);
+        res.json(populatedCart);
+
+    });
+
+    app.get('/products/:productId', async (req, res) => {
+        const productId = req.params.productId;
+        const product = await db.collection('products').findOne({ id : productId});
+        res.json(product);
+
+    });
+
+
+    app.post('/users/:userId/cart', async (req, res) => {
+        const userId = req.params.userId;
+        const productId = req.body.id;
+
+        await db.collection('users').updateOne({ id: userId}, {
+            //tells mongo what type of update- adding productid onto arry of cart items
+            // push: { cartItems: productId }
+            //this makes it so the user can't add duplicates
+            $addToSet: { cartItems: productId} 
+        });
+        const user = await db.collection('users').findOne({ id: req.params.userId});
+        const populatedCart = await populateCartIds(user.cartItems);
+        res.json(populatedCart);
+    });
+
+    app.delete('/users/:userId/cart/:productId', async (req, res) => {
+        const userId = req.params.userId;
+        const productId = req.params.productId;
+        await db.collection('users').updateOne({ id: userId}, {
+            $pull: { cartItems: productId},
+        });
+
+        const user = await db.collection('users').findOne({ id: req.params.userId});
+        const populatedCart = await populateCartIds(user.cartItems);
+        res.json(populatedCart);
+    });
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+
+
 }
 
-app.get('/users/:userId/cart', async (req, res) => {
-    await client.connect();
-    const db = client.db('fsv-db');
-    const user = await db.collection('users').findOne({ id: req.params.userId});
-    const populatedCart = await populateCartIds(user.cartItems);
-    res.json(populatedCart);
-
-});
-
-app.get('/products/:productId', async (req, res) => {
-    await client.connect();
-    const db = client.db('fsv-db');
-    const productId = req.params.productId;
-    const product = await db.collection('products').findOne({ id : productId});
-    res.json(product);
-
-});
-
-
-app.post('/cart', (req, res) => {
-    const data = req.body;
-    cartItems.push(data.id);
-    const populatedCart = populateCartIds(cartItems);
-    res.json(populatedCart);
-});
-
-app.delete('/cart/:productId', (req, res) => {
-    const productId = req.params.productId;
-    cartItems = cartItems.filter(id => id !== productId);
-    const populatedCart = populateCartIds(cartItems);
-    res.json(populatedCart);
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+start();
 
 
 
