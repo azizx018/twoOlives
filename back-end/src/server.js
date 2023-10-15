e0d078511813a26d4aa7bf2ceffdebcbe714cbd8
azizx018 <chiara.aziz@gmail.com>
2023-10-15 17:37:04 -0500

    convert get endpoints

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,4 +1,9 @@
 import express, { request } from 'express';
+import { MongoClient } from  'mongodb';
+require('dotenv').config()
+import 'dotenv/config';
+
+
 
 //just for editing at the moment since there is no db
 import { cartItems as cartItemsRaw, products as productsRaw } from './temp-data';
@@ -6,6 +11,9 @@ import { cartItems as cartItemsRaw, products as productsRaw } from './temp-data'
 let cartItems = cartItemsRaw;
 let products = productsRaw;
 
+const url = `mongodb+srv://fullstack-server:${process.env.DB_PASSWORD}@cluster0.toqxqs8.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(url);
 
 const port = 8000;
 
@@ -13,37 +21,40 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/hello', (req, res) => {
-    res.send("Hello Jello!")
+
+app.get('/products', async (req, res) => {
+    await client.connect();
+    const db = client.db('fsv-db');
+    const products = await db.collection('products').find({}).toArray();
+    res.send(products);
 });
 
-app.get('/products', (req, res) => {
-    res.json(products);
 
-});
-function populateCartIds(ids) {
-    return ids.map(id => products.find(product => product.id === id))
+async function populateCartIds(ids) {
+    await client.connect();
+    const db = client.db('fsv-db');
+    return Promise.all(ids.map(id => db.collection('products').findOne({id})));
     
 }
 
-app.get('/cart', (req, res) => {
-    const populatedCart = populateCartIds(cartItems);
+app.get('/users/:userId/cart', async (req, res) => {
+    await client.connect();
+    const db = client.db('fsv-db');
+    const user = await db.collection('users').findOne({ id: req.params.userId});
+    const populatedCart = await populateCartIds(user.cartItems);
     res.json(populatedCart);
 
 });
 
-app.get('/products/:productId', (req, res) => {
+app.get('/products/:productId', async (req, res) => {
+    await client.connect();
+    const db = client.db('fsv-db');
     const productId = req.params.productId;
-    const product = products.find(product => product.id === productId);
+    const product = await db.collection('products').findOne({ id : productId});
     res.json(product);
 
 });
 
-// app.post('/lulu', (req, res) => {
-//     const data = request.body;
-//     console.log(data.id);
-
-// });
 
 app.post('/cart', (req, res) => {
     const data = req.body;
