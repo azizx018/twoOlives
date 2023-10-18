a5d1142f2504d8a6f8830909fdcd9afe80251528
azizx018 <chiara.aziz@gmail.com>
2023-10-18 14:30:29 -0500

    remove items in cart - request to backend

--- front-end/src/pages/ShoppingCartPage.vue
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -3,7 +3,7 @@
     My Cart
   </h1>
  <div v-if="cartItems.length > 0">
-  <ShoppingCartList :products="cartItems"/>
+  <ShoppingCartList @remove-from-cart="removeFromCart($event)" :products="cartItems"/>
   <button class="checkout-button">Checkout</button>
 </div>
   <div v-if="cartItems.length === 0">You have no items in your cart!</div>
@@ -25,6 +25,16 @@ export default {
     return {
       cartItems:[],
     }
+  },
+  methods: {
+    async removeFromCart(productId) {
+      const response = await axios.delete(`/api/users/12345/cart/${ productId}`);
+      const updatedCart = response.data;
+      this.cartItems = updatedCart;
+
+
+    }
+
   },
   async created() {
     const response = await axios.get('/api/users/12345/cart');
